3c543dab920cb0c77e27920071cf6f97af0e0a59
chaitanya <vchaitanya4545@gmail.com>
2025-06-19 23:30:01 +0530

    module ecr files

---
+++ Terraform/modules/ecr/main.tf
@@ -0,0 +1,11 @@
+#Defining the ECR repository
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project_name}-repo"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "${var.project_name}-repo"
+  }
+}
