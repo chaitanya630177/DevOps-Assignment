0e9e815c6d31d89076817d03a70d0a24df02758e
chaitanya <vchaitanya4545@gmail.com>
2025-06-20 23:11:24 +0530

    updating the user-data

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,4 +1,3 @@
-'''bash
 #!/bin/bash
 # Install Docker
 yum update -y
@@ -29,12 +28,57 @@ cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
         ]
       }
     }
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/docker",
+            "log_group_name": "${log_group_name}",
+            "log_stream_name": "{instance_id}/docker"
+          }
+        ]
+      }
+    }
   }
 }
 EOF
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
+# Install Fluent Bit (for ELK)
+curl https://packages.fluentbit.io/amazonlinux/2/fluent-bit.repo -o /etc/yum.repos.d/fluent-bit.repo
+yum install fluent-bit -y
+cat <<EOF > /etc/fluent-bit/fluent-bit.conf
+[SERVICE]
+    Flush        1
+    Log_Level    info
+    Parsers_File parsers.conf
+
+[INPUT]
+    Name              tail
+    Path              /var/log/docker
+    Parser            json
+
+[OUTPUT]
+    Name              es
+    Match             *
+    Host              elasticsearch
+    Port              9200
+    Index             chaydemo-logs
+    Type              _doc
+EOF
+cat <<EOF > /etc/fluent-bit/parsers.conf
+[PARSER]
+    Name   json
+    Format json
+    Time_Key time
+    Time_Format %Y-%m-%dT%H:%M:%S
+EOF
+systemctl start fluent-bit
+systemctl enable fluent-bit
+
 # Pull and run Docker image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REGISTRY}/${ECR_REPOSITORY}:latest
-docker run -d -p 80:3000 ${ECR_REGISTRY}/${ECR_REPOSITORY}:latest
-```
+docker run -d -p 80:3000 --log-driver=awslogs --log-opt awslogs-region=us-east-1 --log-opt awslogs-group=${log_group_name} --log-opt awslogs-stream={instance_id}/app ${ECR_REGISTRY}/${ECR_REPOSITORY}:latest
+
